fd5ea5c2f91fe0a0a0070f0f07627aef0ef8b1bf
renankling <renankling00@gmail.com>
2025-08-17 19:33:41 -0300

    Adicionada função para exibir o nome dos participantes na tela

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let amigos = [];
 
 let nome = document.getElementById("amigo");
+let lista = document.getElementById("listaAmigos");
 
 function adicionarAmigo(){
     if(nome.value != "" ){
-        amigos.push(nome);
+        amigos.push(nome.value);
         console.log(amigos);
         limparCampo();
+        exibirLista();
     }else{
         alert("Por favor, insira um nome");
     }
@@ -14,4 +16,13 @@ function adicionarAmigo(){
 
 function limparCampo(){
     nome.value = "";
+}
+
+function exibirLista(){
+    lista.innerHTML = "";
+    for(i=0;i<amigos.length;i++){
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
 }
